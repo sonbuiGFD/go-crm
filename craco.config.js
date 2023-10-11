7aa7a33faae0df0109b6b99ec65beaae8c4bd4e9
Son Bui <sonbui@linecorp.com>
2023-10-12 01:24:31 +0700

    update setting

--- craco.config.js
+++ craco.config.js
@@ -1,30 +1,5 @@
 // antd css vars https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 
-const CracoLessPlugin = require('craco-less');
-
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            modifyVars: {
-              'code-family': 'Poppins',
-              'font-size-base': '16px',
-              'font-size-sm': '14px',
-              'primary-color': '#5669ff',
-              'success-color': '#28c76f',
-              'warning-color': '#f3a22c',
-              'error-color': '#f65747',
-              'text-color': '#373737',
-              'body-background': '#f0f3f8',
-              black: '#222222',
-            },
-            javascriptEnabled: true,
-          },
-        },
-      },
-    },
-  ],
+  plugins: [],
 };
